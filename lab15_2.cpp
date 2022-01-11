8ec34b75e8b1565ce35a805736053e04f19e7646
guita13921 <guita13921@gmail.com>
2022-01-11 17:32:35 +0700

    lab15_2.cpp complete

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,35 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool x[][N]){
+    for(int i = 0;i<N;i++){
+        for(int j = 0;j<N;j++){
+            cout << x[i][j] << " ";        
+        }
+    cout << endl;
+    }
+}    
+
+void inputMatrix(double a[][N]){
+    for(int i = 0; i < N; i++){
+        cout << "Row " << i+1 << ": ";
+        for(int j = 0; j < N; j++){
+            cin >> a[i][j];
+        }
+    }
+}
+
+void findLocalMax(const double b[][N], bool c[][N]){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            if(i == N-1 || i == 0 || j == 0 || j == N-1){
+                c[i][j] = 0;
+            }if(b[i+1][j+1]>=b[i+1][j] and b[i+1][j+1] >= b[i][j+1] and b[i+1][j+1] >= b[i+2][j+1] and b[i+1][j+1] >= b[i+1][j+2]){
+                c[i+1][j+1] = 1;
+            }else{
+                c[i+1][j+1] = 0;
+            }
+        }
+    }
+}
